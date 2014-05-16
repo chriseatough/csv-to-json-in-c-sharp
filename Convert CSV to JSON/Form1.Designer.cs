45953f89d40da578b97dcbc98ab5e739bea3d275
eatoug01 <c.eatough@leedsmet.ac.uk>
2014-05-16 16:32:04 +0100

    Added an open file dialog, reading the csv string and converting it to a json string to be output to the textbox

--- Convert CSV to JSON/Form1.Designer.cs
+++ Convert CSV to JSON/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonChooseFile = new System.Windows.Forms.Button();
+            this.textBoxJSON = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonChooseFile
+            // 
+            this.buttonChooseFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonChooseFile.Location = new System.Drawing.Point(13, 13);
+            this.buttonChooseFile.Name = "buttonChooseFile";
+            this.buttonChooseFile.Size = new System.Drawing.Size(259, 23);
+            this.buttonChooseFile.TabIndex = 0;
+            this.buttonChooseFile.Text = "Choose CSV File";
+            this.buttonChooseFile.UseVisualStyleBackColor = true;
+            this.buttonChooseFile.Click += new System.EventHandler(this.buttonChooseFile_Click);
+            // 
+            // textBoxJSON
+            // 
+            this.textBoxJSON.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxJSON.Location = new System.Drawing.Point(15, 42);
+            this.textBoxJSON.Multiline = true;
+            this.textBoxJSON.Name = "textBoxJSON";
+            this.textBoxJSON.Size = new System.Drawing.Size(257, 208);
+            this.textBoxJSON.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBoxJSON);
+            this.Controls.Add(this.buttonChooseFile);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonChooseFile;
+        private System.Windows.Forms.TextBox textBoxJSON;
     }
 }
 
--- Convert CSV to JSON/Form1.cs
+++ Convert CSV to JSON/Form1.cs
@@ -16,5 +16,36 @@ namespace Convert_CSV_to_JSON
         {
             InitializeComponent();
         }
+
+        private void buttonChooseFile_Click(object sender, EventArgs e)
+        {
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "CSV Files|*.csv|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = true;
+
+            DialogResult openFileResult = openFileDialog1.ShowDialog();
+
+            if (openFileResult != DialogResult.OK)
+            {
+                return;
+            }
+
+            System.IO.Stream fileStream = openFileDialog1.OpenFile();
+            string csv = string.Empty;
+
+            using(System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+            {
+                csv = reader.ReadToEnd();
+            }
+
+            fileStream.Close();
+
+            CSVConverter converter = new CSVConverter();
+
+            textBoxJSON.Text = converter.ConvertToJSON(csv);
+        }
+
+
     }
 }
